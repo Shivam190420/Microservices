d63a960532eb306fe15195ea71db498db6d7042c
Shivam190420 <65591059+Shivam190420@users.noreply.github.com>
2022-08-03 01:11:40 +0530

    Add files via upload

---
+++ Day 2/Task 2/WebApplication1/Controllers/OrderController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class OrderController : Controller
+    {
+        private Assignment_DBEntities db = new Assignment_DBEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order Order = db.Orders.Find(id);
+            if (Order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Order);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order Order = db.Orders.Find(id);
+            if (Order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Order);
+        }
+
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order Order = db.Orders.Find(id);
+            if (Order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Order);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order sample = db.Orders.Find(id);
+            db.Orders.Remove(sample);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
